16a3a390dc091b044ea11f3a1ac150b0727f3dc1
WalterFrongia <walter.frongia@libero.it>
2021-10-04 17:47:08 +0200

    Importato axios in app, e utilizzata axios get per richiamare API

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 <script>
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
@@ -18,7 +19,10 @@ export default {
   methods:{
     loadMovies: function(inputMessage){
       console.log(inputMessage);
-      
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=a9f88cb578c09b30f91d0cd1be119282&query=parola')
+      .then(function (response) {
+        console.log(response);
+      })
     }
   },
 }
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,19 @@
 <template>
-    <div>
+    <div id="header-section" class="ps-5 pt-5">
+        <img src="" alt="">
+        <input v-model = "message" placeholder="search">
+        <button @click="$emit('search',message)">search</button>
     </div>
 </template>
 
 <script>
 export default {
+    name:"Header",
+    data: function(){
+        return{
+            message:"",
+        }
+    }
 
 }
 </script>
