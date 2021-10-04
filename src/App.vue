f120b3c92c767cbb4bd6e7a1f875deb86a940d1e
WalterFrongia <walter.frongia@libero.it>
2021-10-04 11:52:30 +0200

    Importazione dei components in App e importazione di bootstrap

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,19 @@
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
-   
+    Header,
+    Main,
   }
 }
 </script>
 
 <style lang="scss">
+@import '~bootstrap/scss/bootstrap';
 
 </style>
