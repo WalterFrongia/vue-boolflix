39db7993a85987ca6b7255a6aa9d7d3fea75b869
WalterFrongia <walter.frongia@libero.it>
2021-10-05 12:47:58 +0200

    Aggiunto array per salvare API, e aggiunta dei props per comunicare array contenente API al componnets content e stampate informazioni API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
     <Header @search="loadMovies"/>,
-    <Main/>,
+    <Content :listaFilm="arrayMovies"/>,
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-import Main from './components/Main.vue'
+import Content from './components/Content.vue'
 import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Main,
+    Content,
+  },
+  data: function(){
+      return{
+        //CREATO ARRAY PER SALVARE API
+        arrayMovies:[],
+      }
   },
   methods:{
     loadMovies: function(inputMessage){
-      console.log(inputMessage);
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=a9f88cb578c09b30f91d0cd1be119282&query=parola')
-      .then(function (response) {
-        console.log(response);
-      })
+      // console.log(inputMessage);
+      if (inputMessage) {
+          axios.get('https://api.themoviedb.org/3/search/movie',{
+          params:{
+            api_key:'a9f88cb578c09b30f91d0cd1be119282',
+            query: inputMessage,
+          }
+        })
+        .then ((response) => {
+          console.log(response);
+          //RICHIAMATO ARRAY CON THIS E ALL'INTERNO SALVATO API CON I DATI CHE CI INTERESSANO 
+          this.arrayMovies = (response.data.results);
+          console.log(this.arrayMovies);
+        })
+      }
     }
   },
 }
---
+++ src/components/Content.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="card-container">
+        <div class="card-element" 
+        v-for="(card, index) in listaFilm" :key="index">
+        <ul>
+            <li>{{card.title}}</li>
+        </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Content",
+    props:["listaFilm"]
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
